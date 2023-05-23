3bc35427cf60272d7f2c3dad04a54f17a6bf6e91
AnikHaque <ekramul15-811@diu.edu.bd>
2023-05-24 00:30:59 +0600

    firebase added

--- src/components/Firebase/Firebase.init.js
+++ src/components/Firebase/Firebase.init.js
@@ -2,17 +2,13 @@
 import { initializeApp } from "firebase/app";
 // TODO: Add SDKs for Firebase products that you want to use
 
-
-
-
-// Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyC1MkFVH7obOHSXSKsx-JBx1QnnJurqGEM",
-  authDomain: "edu-genius.firebaseapp.com",
-  projectId: "edu-genius",
-  storageBucket: "edu-genius.appspot.com",
-  messagingSenderId: "226792863475",
-  appId: "1:226792863475:web:8896a345df8cc41c946986"
+  apiKey: "AIzaSyDwwyKBVJFIi2MKMCyivj6yFoIOSZdhotk",
+  authDomain: "edu-genius-45cb2.firebaseapp.com",
+  projectId: "edu-genius-45cb2",
+  storageBucket: "edu-genius-45cb2.appspot.com",
+  messagingSenderId: "1035496111180",
+  appId: "1:1035496111180:web:b93d7eac60d3e352fcc8e9"
 };
 
 // Initialize Firebase
